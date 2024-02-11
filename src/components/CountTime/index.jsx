9054cb3fd3e2d97b8b621bfab0ab9ee51399a15d
Nhat Nguyen Van (F&B) <nhat.nguyen.van@stepmediasoftware.com>
2024-02-11 22:31:46 +0700

    upload

---
+++ src/components/CountTime/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './CountTime.css';
+
+const padZero = (num) => {
+  return (num < 10 ? '0' : '') + num;
+};
+
+function CountTime() {
+
+  const [now, setNow] = useState(new Date() - new Date("2019/09/19"));
+
+  const days = Math.floor(now / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((now % (1000 * 60)) / 1000);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date() - new Date("2019/09/19"));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="count-time__wrapper">
+      <div className='infor'>
+        <div className='infor__item heart'>
+          <div className='name'>Nhật</div>
+          <div className='birth'>16-04</div>
+        </div>
+        <div className='infor__item heart'>
+          <div className='name'>Hậu</div>
+          <div className='birth'>21-12</div>
+        </div>
+      </div>
+      <div className="count-time">
+        <div className="count-time__item days">
+          <div className="digit">{padZero(days)}</div>
+          <div className="label">Days</div>
+        </div>
+        <div className="count-time__item hours">
+          <div className="digit">{padZero(hours)}</div>
+          <div className="label">Hours</div>
+        </div>
+        <div className="count-time__item minutes">
+          <div className="digit">{padZero(minutes)}</div>
+          <div className="label">Minutes</div>
+        </div>
+
+        <div className="count-time__item seconds">
+          <div className="digit">{padZero(seconds)}</div>
+          <div className="label">Seconds</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CountTime;
